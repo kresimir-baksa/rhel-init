f7dbedbee1e0bba56547c7054ccf55671826e116
Kresimir Baksa <baxa.kresimir@gmail.com>
2024-07-03 15:03:05 +0200

    refactor: Changed src structure

--- src/scripts/30-terminal-color.sh
+++ src/scripts/30-terminal-color.sh
@@ -4,6 +4,7 @@ INFO_MESSAGE="Rice bash terminal"
 FINISHED_MESSAGE="Terminal ricing finished"
 
 BASH_PROMPT_PATH="$(cd "$(readlink -f $(dirname "$0"))/../.." && pwd)/config/bash-prompt"
+UTILS_PATH="$(cd "$(readlink -f $(dirname "$0"))/../utils" && pwd)"
 
 # Parse the input options
 while getopts "i" opt; do
@@ -25,38 +26,10 @@ done
 sudo cp "$BASH_PROMPT_PATH" /etc/profile.d/prompt.sh
 
 # Duplicate current prifle to enable gogh script run
-# Get the current default profile UUID
-current_default=$(dconf read /org/gnome/terminal/legacy/profiles:/default | tr -d "'")
-
-# Generate a new UUID for the duplicate profile
-new_uuid=$(uuidgen)
-
-# Duplicate the current profile
-dconf dump /org/gnome/terminal/legacy/profiles:/:$current_default/ | dconf load /org/gnome/terminal/legacy/profiles:/:$new_uuid/
-
-# Set the visible name for the new profile
-dconf write /org/gnome/terminal/legacy/profiles:/:$new_uuid/visible-name "'new_duplicate'"
-
-# Set the new profile as the default
-dconf write /org/gnome/terminal/legacy/profiles:/default "'$new_uuid'"
-
-echo "Duplicated profile $current_default to $new_uuid and set it as default."
+bash "$UTILS_PATH/duplicate-term-theme.sh"
 
 # Run gogh
-# Clone the repo into "$HOME/src/gogh"
-mkdir -p "$HOME/src"
-cd "$HOME/src"
-git clone https://github.com/Gogh-Co/Gogh.git gogh
-cd gogh
-
-# necessary in the Gnome terminal on ubuntu
-export TERMINAL=gnome-terminal
-
-# Enter theme installs dir
-cd installs
-
-# install themes
-./gruvbox-dark.sh
+bash "$UTILS_PATH/gogh.sh"
 
 # Cleanup
 rm -rf "$HOME/src"
