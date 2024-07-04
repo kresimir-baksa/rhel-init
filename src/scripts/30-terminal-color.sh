818c09eb1d3d2e3368073fb0c54a9fdfb392384d
Kresimir Baksa <baxa.kresimir@gmail.com>
2024-07-04 18:13:08 +0200

    fix: Simplify terminal color scheme

--- src/scripts/30-terminal-color.sh
+++ src/scripts/30-terminal-color.sh
@@ -6,6 +6,7 @@ FINISHED_MESSAGE="Terminal ricing finished"
 BASH_PROMPT_PATH="$(cd "$(readlink -f $(dirname "$0"))/../.." && pwd)/config/bash-prompt"
 UTILS_PATH="$(cd "$(readlink -f $(dirname "$0"))/../utils" && pwd)"
 TMUX_CONFIG_PATH="$(cd "$(readlink -f $(dirname "$0"))/../.." && pwd)/config/tmux.conf"
+COLOR_SCHEME_PATH=$(cd "$(readlink -f $(dirname "$0"))/../.." && pwd)/config/gruvbox.conf"
 
 minimal_install=false
 if [[ $(hostname) == server* ]];
@@ -34,16 +35,14 @@ sudo cp "$BASH_PROMPT_PATH" /etc/profile.d/prompt.sh
 sudo cp "$TMUX_CONFIG_PATH" /etc/tmux.conf
 
 if [ "$minimal_install" = false ]; then
-    # Duplicate current prifle to enable gogh script run
-    bash "$UTILS_PATH/duplicate-term-theme.sh"
+    # Generate a new UUID for the duplicate profile
+    new_uuid=$(uuidgen)
 
-    sleep 1
+    # Load new color scheme config
+    cat "$COLOR_SCHEME_PATH" | dconf load /org/gnome/terminal/legacy/profiles:/:$new_uuid/
 
-    # Run gogh
-    bash "$UTILS_PATH/gogh.sh"
-
-    # Cleanup
-    rm -rf "$HOME/src"
+    # Set the new profile as the default
+    dconf write /org/gnome/terminal/legacy/profiles:/default "'$new_uuid'"
 fi
 
 echo "$FINISHED_MESSAGE"
