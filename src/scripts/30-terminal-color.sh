e95adf56b6c5c5519936b8a987478434c0c5a33b
Kresimir Baksa <baxa.kresimir@gmail.com>
2024-07-05 12:22:06 +0200

    fix: Remove comments from script

--- src/scripts/30-terminal-color.sh
+++ src/scripts/30-terminal-color.sh
@@ -46,20 +46,3 @@ if [ "$minimal_install" = false ]; then
 fi
 
 echo "$FINISHED_MESSAGE"
-
-
-
-# # Clone the repo into "$HOME/src/gogh"
-# mkdir -p "$HOME/src"
-# cd "$HOME/src"
-# git clone https://github.com/Gogh-Co/Gogh.git gogh
-# cd gogh
-
-# # necessary in the Gnome terminal on ubuntu
-# export TERMINAL=gnome-terminal
-
-# # Enter theme installs dir
-# cd installs
-
-# # install themes
-# ./gruvbox-dark.sh
